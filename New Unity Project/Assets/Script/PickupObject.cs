0bff332baf4d9d6562af56ff546a9497e68c7aa5
123tris <xonepiece4everx@gmail.com>
2016-03-26 03:48:31 +0100

    HUD update & Fading

---
+++ New Unity Project/Assets/Script/PickupObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+    private Transform player;
+    UIController uiMessage;
+
+    bool ObjectHighlighted;
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        uiMessage = GameObject.Find("HUD").GetComponent<UIController>();
+    }
+    void Update() {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            if (objectHit.tag == "Interactable") uiMessage.DisplayText("E to interact");
+            else uiMessage.DisplayText("");
+        }
+    }
+}
