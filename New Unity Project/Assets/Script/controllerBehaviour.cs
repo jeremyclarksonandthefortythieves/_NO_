c39aef85ffdfe1fde987bdd6ec2cbc258acf28a3
howlmortar <juliandealdrey@gmail.com>
2016-04-08 16:24:17 +0200

    BWEH and ducklings
    changed the controller at the end and gave it some animations on trigger

---
+++ New Unity Project/Assets/Script/controllerBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controllerBehaviour : MonoBehaviour {
+
+    public Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+}
---
+++ New Unity Project/Assets/Script/controllerTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controllerTrigger : MonoBehaviour {
+
+    public controllerBehaviour doorAnim;
+
+    void Start() {
+        doorAnim = FindObjectOfType<controllerBehaviour>();
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.name == "Player") {
+            doorAnim.anim.SetInteger("doorBehaviour", 1);
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if (other.name == "Player") {
+            doorAnim.anim.SetInteger("doorBehaviour", 2);
+        }
+    }
+}
