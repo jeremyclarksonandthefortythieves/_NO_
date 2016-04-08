033870ff07f370d704c93aa47fff4432df53e48b
howlmortar <advans77advans77>
2016-04-08 21:39:07 +0200

    pretty lights n shit BWEH
    added the particle system to the controller and light intensify over
    time.

---
+++ New Unity Project/Assets/Script/controllerEnterBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controllerEnterBehaviour : MonoBehaviour {
+
+    public controllerBehaviour doorAnim;
+	public GameObject particles1;
+	public GameObject particles2;
+	public Light light;
+	private float fading = 0f;
+	public float fadeRate = 10f;
+	public float fadeTo = 2f;
+	private bool lightStart = false;
+
+    void Start()
+    {
+        doorAnim = FindObjectOfType<controllerBehaviour>();
+    }
+
+	void Update()
+	{
+		if (lightStart) {
+			fading += Time.deltaTime * fadeRate;
+			light.intensity = Mathf.Lerp (.2f,fadeTo,fading);
+		}
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            doorAnim.anim.SetInteger("doorBehaviour", 2);
+			particles1.SetActive (false);
+			particles2.SetActive (true);
+			lightStart = true;
+        }
+    }
+}
